e342e972463f877e8adc406661e3e49cbf745620
Luis Osvaldo <aldolp40@gmail.com>
2019-08-23 21:47:52 -0500

    arrays y js

---
+++ 04_IntroJS/main.js
@@ -0,0 +1,47 @@
+// alert("si funciona XDDDDD")
+// ---declarando variables----
+
+// let numero = 23;
+// let texto = "Hola XD";
+// let booleano = true;
+// let array = [19 , "probando el array XD", false];
+// let object = {
+//     nombre: "Luis",
+//     trabajo: "Dev.f",
+//     edad: 20
+// }
+
+// let x = 13;
+// let y = 5;
+// let suma = 23 + 98;
+// let sumaDeVariables = x + y; 
+
+// console.log("Hola desde la consola!!! XDD");
+// console.log(suma);
+// console.log(sumaDeVariables);
+
+// x = 1;
+// y = 4;
+
+// sumaDeVariables = x - y;
+
+// console.log(sumaDeVariables)
+
+
+// let x = Number(prompt("Ingrese el primer valor"));
+
+
+// let y = Number(prompt("Ingrese el segundo valor"));
+
+// let suma = x + y;
+
+// console.log(suma);
+
+let nombre = prompt("Como te llamas, man")
+let edad = prompt("Ingresa tu edad, man");
+let ocupacion = prompt("A qué te dedicas ,man");
+
+let resultado = nombre + " " + edad + " " + ocupacion;
+
+console.log(resultado)
+
---
+++ 05_Arreglos/main.js
@@ -0,0 +1,9 @@
+let animales = ["salchicha", "cocodrilo", "pajarito", "hipo", "caballo"];
+
+let nombres = ["Quentin" , "Roberto" , "Azamath", "Parangaricutirimicuaro" , "Luis XD"]
+
+console.log(animales[1] , nombres[4]);
+
+animales[5] = "gato"
+
+console.log(animales)
